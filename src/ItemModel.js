3bab98c01b509f3a3e0c3c034c7ecddc2a86e6b3
kiselevgleb <kiselevgleb@gmail.com>
2020-09-03 19:05:07 +0300

    added types

---
+++ src/ItemModel.js
@@ -0,0 +1,13 @@
+
+class ItemModel {
+  constructor(prop){
+    this.brand = prop.brand;
+    this.title = prop.title;
+    this.description = prop.description;
+    this.descriptionFull = prop.descriptionFull;
+    this.price = prop.price;
+    this.currency = prop.currency;
+  }
+}
+
+export default ItemModel;
